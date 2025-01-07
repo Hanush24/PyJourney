5ba3304bb98498a7bdd2b49c5230802f9368605e
Hanush24 <hanushshanmugam24@gmail.com>
2025-01-07 18:55:13 +0530

    Add Python script for casting between data types

---
+++ casting.py
@@ -0,0 +1,16 @@
+#printing without double quotes 
+a=10
+b=32
+#printing with double quotes - STRING
+c="10"
+d="32"
+print(a+b)
+#sting+string=string
+print(c+d)
+
+#casting = converting one datatype into another datatype
+#if i give input in STRING and output in INTEGER use this 
+num1=int("10")
+num2=int("32")
+sum=num1+num2
+print(sum)

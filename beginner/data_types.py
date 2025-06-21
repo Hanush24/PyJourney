e9eee9dcb23aa7b983ffe2972c0f3f56ac4969ea
Hanush24 <hanushshanmugam24@gmail.com>
2025-06-21 16:17:54 +0530

    practised datatypes and variables

---
+++ beginner/data_types.py
@@ -0,0 +1,11 @@
+num =10 #int
+price = 99.99 #float
+is_on = False #bool
+text = "Hello" #str
+items = [1,2,3] #list
+tools = ("pen", "pad") #tupule
+person = {"name": "Hanush", "age": 21} #dict
+unique = {1, 2, 3} #set
+
+print(type(num), type(price), type(is_on), type(text))
+print(type(items), type(tools), type(person), type(unique))
---
+++ beginner/variables.py
@@ -0,0 +1,10 @@
+# variables.py
+
+name = "Hanush S N"
+age = 21
+is_engineer = True
+
+#comments are ignored
+#this is a single line comment
+
+print(f"name: {name}, Age: {age}, Studied: {is_engineer}")
